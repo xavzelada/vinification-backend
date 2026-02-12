3e5c64feca71b30a6d00a6bc1dc66de5500a3d97
Xavier Vasquez <xavzelada@gmail.com>
2026-02-12 16:53:42 +0300

    Add NelmioCorsBundle with basic CORS settings and bundle configuration

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
